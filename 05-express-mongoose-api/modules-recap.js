87bcead30b6f5c85e2a59c78450a5a2a53c2acf3
losrobbos <losrobbos@web.de>
2021-08-31 12:05:18 +0200

    Removed unnecessary file

--- 05-express-mongoose-api/modules-recap.js
+++
@@ -1,7 +0,0 @@
-import react from 'react' // default export
-// export default UserProfile
-
-import { myFanceFunc, MY_API_URL } from 'react' // named exports
-// export const myFancyFunc = () => {...}
-// export const MY_API_URL = "http//whatever.com"
-

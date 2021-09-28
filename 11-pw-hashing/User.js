6a89b82c2cca148def9cd813a905c5bd89c899c3
losrobbos <losrobbos@web.de>
2021-09-28 11:45:59 +0200

    Added Bcrypt hashing & compare

--- 11-pw-hashing/User.js
+++ 11-pw-hashing/User.js
@@ -4,8 +4,17 @@ const { Schema, model } = mongoose
 const UserSchema = new Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
-  password: { type: String, required: true }
-})
+  password: { type: String, required: true } // find against => leave out the PW!
+}, 
+{
+  // hide fields from all OUTPUTs / reponses !
+  toJSON: {
+    transform: (original, returnedDoc) => {
+      delete returnedDoc.password;
+    }
+  }
+}
+)
 
 const User = model("User", UserSchema)
 
--- 11-pw-hashing/server.js
+++ 11-pw-hashing/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors'
 import './db-connect.js'
 import User from './User.js'
+import bcrypt from 'bcryptjs'
 
 const app = express();
 
@@ -14,20 +15,45 @@ app.get('/', (req, res) => {
 
 app.post("/signup", async (req, res, next) => {
 
-  const userCreated = await User.create(req.body)
-  res.json( userCreated )
+  const userData = { ...req.body }
+
+  // salt round = 10 => the password will have 10 rounds (!) of hashing!
+  userData.password = bcrypt.hashSync( userData.password, 10 ) // salt round => cost factor!
+
+  const userCreated = await User.create(userData)
+  res.json( userCreated ) // => res.json => toJSON()
 })
 
 app.post("/login", async (req, res, next) => {
 
-  const {email, password} = req.body
-  const foundUser = await User.findOne({ email, password })
+  const {email, password} = req.body // password: hello123
+  const foundUserInDb = await User.findOne({ email })
 
-  if(!foundUser) {
-    next(new Error("User does not exist"))
+  // a record by ID: findById
+  // filter for ONE record by other criteria => findOne
+
+  if(!foundUserInDb) {
+    return next(new Error("User does not exist"))
   }
 
-  res.json( foundUser )
+  // user found by email!!
+
+  // check password => compare PLAIN password of user with HASH
+
+  // hello123
+  // bcrypt => hello123 => hash => 12234565 => 12234565 => LOGIN!
+  const loginSuccessful = bcrypt.compareSync( password, foundUserInDb.password )
+
+  if(!loginSuccessful) {
+    return next( new Error("Passwords don't match, freak") )
+  }
+ 
+  res.json( foundUserInDb ) // null
+})
+
+// GENERAL ERROR HANDLER
+app.use( (err, req, res, next) => {
+  res.status(400).json({ error: err.message })
 })
 
 const PORT = 5000

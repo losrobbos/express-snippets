bedbd2836638c658069fbc52d440190aeb06f227
losrobbos <50549469+losrobbos@users.noreply.github.com>
2022-05-12 17:25:30 +0200

    Update db-connect.js

--- 05-express-mongoose-api/db-connect.js
+++ 05-express-mongoose-api/db-connect.js
@@ -2,7 +2,6 @@ import mongoose from 'mongoose' // CommonJS module
 const { connect } = mongoose // pluck out what we need
 
 const MONGO_URI = "mongodb://localhost/users_db"
-//const MONGO_URI = "mongodb+srv://rob123:<password>@robclusterunlimited.3dzba.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 // Connection is established at BEGINNING of our program
 // and stays alive (=> hangs around waiting for DB operations)
